bc97f7b5cd1d73607968777050fc5e21201f4cbe
Oleksandr <lozikanet@gmail.com>
2021-10-08 12:24:28 +0300

    task-2-change

--- js/task-02.js
+++ js/task-02.js
@@ -7,11 +7,19 @@ const ingredients = [
   "Condiments",
 ];
 
-let ingredientsList = document.getElementById("ingredients");
+// let ingredientsList = document.getElementById("ingredients");
+// console.log(ingredientsList);
+// const food = ingredients.forEach((ingredient) => {
+//   let items = document.createElement("li");
+//   items.innerText = ingredient;
+//   items.classList.add("item");
+//   ingredientsList.append(items);
+// });
+
+const ingredientsList = document.getElementById("ingredients");
 console.log(ingredientsList);
-const food = ingredients.forEach((ingredient) => {
-  let items = document.createElement("li");
-  items.innerText = ingredient;
-  items.classList.add("item");
-  ingredientsList.append(items);
+ingredients.forEach((ingredient) => {
+  const items = document.createElement("li");
+  items.textContent = ingredient;
+  ingredientsList.appendChild(items);
 });
