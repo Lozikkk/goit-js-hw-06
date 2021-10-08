6e7845c0a36b0c2b287b0bab7fa6f6772efe6330
Oleksandr <lozikanet@gmail.com>
2021-10-08 12:15:15 +0300

    task-1-change

--- js/task-01.js
+++ js/task-01.js
@@ -1,14 +1,18 @@
-'use strict';
+// const totalCategories = document.querySelectorAll(".item");
+// console.log(`Number of categories: ${totalCategories.length}`);
+// const categoriesTitle = [...totalCategories]
+//   .map(
+//     (categories) =>
+//       `Category: ${categories.children[0].textContent},
+//     Elements: ${categories.children[1].children.length}`
+//   )
+//   .join("\n");
+// console.log(categoriesTitle);
 
-const totalCategories = document.querySelectorAll('.item');
-console.log(
-  `Number of categories: ${totalCategories.length}`,
-);
-const categoriesTitle = [...totalCategories]
-  .map(
-    categories =>
-      `Category: ${categories.children[0].textContent}, 
-    Elements: ${categories.children[1].children.length}`,
-  )
-  .join('\n');
-console.log(categoriesTitle);
+const totalCategories = document.querySelector("#categories");
+console.log(`Number of categories: ${totalCategories.children.length}`);
+const categoriesTitle = document.querySelectorAll(".item");
+categoriesTitle.forEach((c) => {
+  console.log(`Category: ${c.firstElementChild.textContent},
+  Elements: ${c.lastElementChild.childElementCount}`);
+});
